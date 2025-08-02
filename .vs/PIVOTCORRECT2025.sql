c1e0bbae1c1863ec687acd34c4d137fac67d56bc
trendleader <philjones820@icloud.com>
2025-08-02 18:52:07 -0400

    Update PIVOTCORRECT2025.sql
    Updated Pivot to Provider

--- .vs/PIVOTCORRECT2025.sql
+++ .vs/PIVOTCORRECT2025.sql
@@ -1,12 +1,12 @@
 
 SELECT *
 FROM (
-    SELECT year(date_of_service) AS SvcYr, Type_of_Service, paid_amount
+    SELECT year(date_of_service) AS SvcYr, [Provider_Type], paid_amount
     FROM dbo.claims_data
 ) AS SourceTable
 PIVOT (
     SUM(paid_amount)
-    FOR type_of_service IN ([Inpatient], [Outpatient], [Professional])
+    FOR [Provider_Type] IN (Facility, Physician)
 ) AS PivotTable;
 
 
